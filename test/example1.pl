28a302ec3b9dd2e758ac65e96bc3c672c9500046
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2023-08-15 17:11:05 +0100

    add examples

---
+++ test/example1.pl
@@ -0,0 +1,55 @@
+% ['If people perform in school talent shows often, then they attend and are very engaged with school events.',
+%  'People either perform in school talent shows often or are inactive and disinterested members of their community.', 
+%  'If people chaperone high school dances, then they are not students who attend the school.', 
+%  'All people who are inactive and disinterested members of their community chaperone high school dances.',
+%   'All young children and teenagers who wish to further their academic careers and educational opportunities are students who attend the school.', 
+%   'Bonnie either both attends and is very engaged with school events and is a student who attends the school, or she neither attends and is very engaged with school events nor is a student who attends the school. ']
+% ['If people perform in school talent shows often, then they attend and are very engaged with school events.', 'People either perform in school talent shows often or are inactive and disinterested members of their community.', 'If people chaperone high school dances, then they are not students who attend the school.', 'All people who are inactive and disinterested members of their community chaperone high school dances.', 'All young children and teenagers who wish to further their academic careers and educational opportunities are students who attend the school.', 'Bonnie either both attends and is very engaged with school events and is a student who attends the school, or she neither attends and is very engaged with school events nor is a student who attends the school. ']	['∀x (TalentShows(x) → Engaged(x))', '∀x (TalentShows(x) ∨ Inactive(x))', '∀x (Chaperone(x) → ¬Students(x))', '∀x (Inactive(x) → Chaperone(x))', '∀x (AcademicCareer(x) → Students(x))', '(Engaged(bonnie) ∧ Students(bonnie)) ⊕ (¬Engaged(bonnie) ∧ ¬Students(bonnie))']	If Bonnie is either both a young child or teenager who wishes to further her academic career and educational opportunities and chaperones high school dances or neither is a young child nor teenager who wishes to further her academic career and educational opportunities, then Bonnie is either a student who attends the school or is an inactive and disinterested member of the community.	AcademicCareer(bonnie) ⊕ Chaperone(bonnie) → AcademicCareer(bonnie) ⊕ Inactive(bonnie)	True
+% ['If people perform in school talent shows often, then they attend and are very engaged with school events.', 'People either perform in school talent shows often or are inactive and disinterested members of their community.', 'If people chaperone high school dances, then they are not students who attend the school.', 'All people who are inactive and disinterested members of their community chaperone high school dances.', 'All young children and teenagers who wish to further their academic careers and educational opportunities are students who attend the school.', 'Bonnie either both attends and is very engaged with school events and is a student who attends the school, or she neither attends and is very engaged with school events nor is a student who attends the school. ']	['∀x (TalentShows(x) → Engaged(x))', '∀x (TalentShows(x) ∨ Inactive(x))', '∀x (Chaperone(x) → ¬Students(x))', '∀x (Inactive(x) → Chaperone(x))', '∀x (AcademicCareer(x) → Students(x))', '(Engaged(bonnie) ∧ Students(bonnie)) ⊕ (¬Engaged(bonnie) ∧ ¬Students(bonnie))']	If Bonnie either chaperones high school dances or, if she does not, she performs in school talent shows often, then Bonnie is both a young child or teenager who wishes to further her academic career and educational opportunities and an inactive and disinterested member of the community.	Chaperone(bonnie) ⊕ TalentShows(bonnie) → AcademicCareer(bonnie) ∧ Inactive(bonnie))	False
+% ['∀x (TalentShows(x) → Engaged(x))', 
+% '∀x (TalentShows(x) ∨ Inactive(x))', 
+% '∀x (Chaperone(x) → ¬Students(x))', '
+% ∀x (Inactive(x) → Chaperone(x))', 
+% '∀x (AcademicCareer(x) → Students(x))', 
+% '(Engaged(bonnie) ∧ Students(bonnie)) ⊕ (¬Engaged(bonnie) ∧ ¬Students(bonnie))']	
+
+
+% Bonnie performs in school talent shows often.	
+
+% Engaged(bonnie)	
+
+% Uncertain
+
+%Shows complex FOL including non-Horn cluases, equivalenc erules (blocked in Datalog due to looping)
+
+:- working_directory(_, '../code').
+:-[main].
+
+theoryName(talentshow).
+
+
+axiom([-talentShows(\x),+engaged(\x)]).
+axiom([+talentShows(\y),+inactive(\y)]).
+axiom([-chaperone(\z),-students(\z)]).
+axiom([-inactive(\a), +chaperone(\a)]).
+axiom([-academicCareer(\b), +students(\b)]).
+axiom([-engaged(bonnie),+students(bonnie)]).
+axiom([-students(bonnie),+engaged(bonnie)]).
+
+trueSet([engaged(bonnie)]). %1 insuff
+falseSet([]).
+protect([]).
+heuristics([]).
+
+theoryFile:- pass.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Golden Standard
+% axiom([+academicCareer(bonnie)]).
+% axiom([-talentShows(\x),+engaged(\x)]).
+% axiom([+talentShows(\y),+inactive(\y)]).
+% axiom([-chaperone(\z),-students(\z)]).
+% axiom([-inactive(\a), +chaperone(\a)]).
+% axiom([-academicCareer(\b), +students(\b)]).
+% axiom([-engaged(bonnie),+students(bonnie)]).
+% axiom([-students(bonnie),+engaged(bonnie)]).
---
+++ test/example5.pl
@@ -0,0 +1,64 @@
+% ['A podcast is not a novel.', 
+% '[BG] If a person is born in American City, the person is American.', 
+% '[BG] If a book is novel and it is written by a person, then the person is a novel writer.', 
+% 'Dani Shapiro is an American writer.', 
+% 'Family History is written by Dani Shapiro.', 
+% 'Family History is a novel written in 2003.', 
+% 'Dani Shapiro created a podcast called Family Secrets.', 
+% '[BG] Boston is an American city.']	
+% ['∀x (IsPodcast(x) → ¬IsNovel(x))', 
+% '∀x ∃y (BornIn(x, y) ∧ IsCity(y) ∧ IsAmerican(y) → IsAmerican(x))', 
+% '∀x ∀y (IsNovel(x) ∧ WrittenBy(x, y) → WritesNovel(y))',
+%  'IsAmerican(dani_Shapiro) ∧ IsWriter(dani_Shapiro)', 
+%  'WrittenBy(family_History, dani_Shapiro)', '
+%  IsNovel(family_History) ∧ WrittenIn(family_History, y2003)', 
+%  'IsPodcast(family_Secrets) ∧ CreatedBy(family_Secrets, dani_Shapiro)', 
+%  'IsCity(boston) ∧ IsAmerican(boston)']	
+ 
+%  Dani Shapiro is a novel writer.	WritesNovel(dani_Shapiro)	True
+%  Dani Shapiro was born in Boston.	BornIn(dani_Shapiro, boston)	Uncertain
+%  Family Secrets is a novel.	IsNovel(family_Secrets)	False
+
+
+
+%Examples with functions
+:- working_directory(_, '../code').
+:-[main].
+
+theoryName(authors).
+
+
+axiom([-isPodCast(\x), -isNovel(\x)]).
+axiom([-bornIn(\y,f(\y)),-isCity(f(\y)),-isAmerican(f(\y)),+isAmerican(\y)]).
+axiom([-isNovel(\a),-writtenBy(\a,\b),+writesNovel(\b)]).
+axiom([+isAmerican(dani)]).
+axiom([+isWriter(dani)]).
+axiom([+writtenBy(familyHistory,dani)]).
+axiom([+isNovel(familyHistory)]).
+axiom([+writtenIn(familyHistory,y2003)]).
+axiom([+isPodCast(familySecrets)]).
+axiom([+createdBy(familySecrets,dani)]).
+axiom([+isCity(boston)]).
+axiom([+isAmerican(boston)]).
+
+eqAxiom([f(dani),boston]).
+
+trueSet([bornIn(dani,boston)]). %1 insuff, 1 incomp
+falseSet([writesNovel(dani)]).
+protect([]).
+heuristics([]).
+
+theoryFile:- pass.
+%GS
+% axiom([+bornIn(dani,f(dani))]). %further: generalize
+% axiom([-isPodCast(\x), -isNovel(\x)]).
+% axiom([-bornIn(\y,f(\y)),-isCity(f(\y)),-isAmerican(f(\y)),+isAmerican(\y)]).
+% axiom([-isNovel(\a),-writtenBy(\a,\b),+writesNovel(\b)]).
+% axiom([+isAmerican(dani)]).
+% axiom([+isWriter(dani)]).
+% axiom([+writtenBy(familyHistory,dani)]).
+% axiom([+writtenIn(familyHistory,y2003)]).
+% axiom([+isPodCast(familySecrets)]).
+% axiom([+createdBy(familySecrets,dani)]).
+% axiom([+isCity(boston)]).
+% axiom([+isAmerican(boston)]).
