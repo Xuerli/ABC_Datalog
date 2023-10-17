9ba05634bdc6342a3095968be1c6e8d513621d0f
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2023-10-17 15:29:18 +0100

    Update main.pl

--- src/main.pl
+++ src/main.pl
@@ -1,17 +1,17 @@
 
 abc :-
-    \+current_predicate(logic/1),
+    \+current_predicate(logic/1),!,
     write('No logic specified.'),nl.
 
 abc :-
-    logic(fol),
+    logic(fol),!,
     write('Running FOL version...'),nl,
     working_directory(_,'./ABC_FOL'),
     [abc],
     main.
 
 abc :-
-    logic(datalog),
+    logic(datalog),!,
     write('Running Datalog Version...'),nl,
     working_directory(_,'./ABC_Datalog'),
     [abc],
